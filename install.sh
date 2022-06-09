1f4dbfb4464cf368f7e90ca7c85f2e0e12040bea
Alexander <ancorwin@gmail.com>
2022-06-09 00:55:00 -0700

    installer better

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ SERVICE_NAME="nmg-league-bot"
 SERVICE_PATH="/lib/systemd/system/$SERVICE_NAME.service"
 NGINX_TARGET_PATH="/etc/nginx/conf.d/$SERVICE_NAME.conf"
 cargo build
-npx tailwindcss -i ./tailwind_input.css -o ./static/css/tailwind.css
+npx tailwindcss -i ./tailwind_input.css -o ./http/static/css/tailwind.css
 sudo cp "conf_files/lib/systemd/system/$SERVICE_NAME.service" $SERVICE_PATH
 if [ -f $NGINX_TARGET_PATH ]; then
   echo "$NGINX_TARGET_PATH already exists; not overwriting"
