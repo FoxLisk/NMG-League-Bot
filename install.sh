35dfb5feb13e1d655419781e46cc328aaf111f82
Alexander <ancorwin@gmail.com>
2022-06-09 00:29:02 -0700

    WIP

---
+++ install.sh
@@ -0,0 +1,15 @@
+set -e
+SERVICE_NAME="nmg_league"
+SERVICE_PATH="/lib/systemd/system/$SERVICE_NAME.service"
+NGINX_TARGET_PATH="/etc/nginx/conf.d/$SERVICE_NAME.conf"
+cargo build
+npx tailwindcss -i ./tailwind_input.css -o ./static/css/tailwind.css
+sudo cp "conf_files/lib/systemd/system/$SERVICE_NAME.service" $SERVICE_PATH
+if [ -f $NGINX_TARGET_PATH ]; then
+  echo "$NGINX_TARGET_PATH already exists; not overwriting"
+else
+  sudo cp "conf_files/etc/nginx/conf.d/$SERVICE_NAME.conf" $NGINX_TARGET_PATH
+fi
+sudo systemctl daemon-reload
+sudo systemctl restart "$SERVICE_NAME"
+sudo systemctl enable "$SERVICE_NAME"
