1644b174d873b57c73c95c573a8672be476f9cd0
Alexander <ancorwin@gmail.com>
2022-06-17 01:33:41 -0700

    forfeit confirmation

--- src/discord/bot.rs
+++ src/discord/bot.rs
@@ -1,11 +1,7 @@
 use crate::constants::{APPLICATION_ID_VAR, TOKEN_VAR};
 use crate::db::get_pool;
 use crate::discord::discord_state::DiscordState;
-use crate::discord::{
-    interactions, CUSTOM_ID_FINISH_RUN, CUSTOM_ID_FORFEIT_RUN, CUSTOM_ID_START_RUN,
-    CUSTOM_ID_USER_TIME, CUSTOM_ID_USER_TIME_MODAL, CUSTOM_ID_VOD, CUSTOM_ID_VOD_MODAL,
-    CUSTOM_ID_VOD_READY,
-};
+use crate::discord::{interactions, CUSTOM_ID_FINISH_RUN, CUSTOM_ID_FORFEIT_RUN, CUSTOM_ID_START_RUN, CUSTOM_ID_USER_TIME, CUSTOM_ID_USER_TIME_MODAL, CUSTOM_ID_VOD_MODAL_INPUT, CUSTOM_ID_VOD_MODAL, CUSTOM_ID_VOD_READY, CUSTOM_ID_FORFEIT_MODAL, CUSTOM_ID_FORFEIT_MODAL_INPUT};
 use crate::models::race::{NewRace, Race};
 use crate::models::race_run::RaceRun;
 use crate::{Shutdown, Webhooks};
@@ -42,6 +38,8 @@ use twilight_model::id::marker::{ApplicationMarker, MessageMarker, UserMarker};
 use twilight_model::id::Id;
 use twilight_util::builder::command::CommandBuilder;
 use twilight_util::builder::InteractionResponseDataBuilder;
+use lazy_static::lazy_static;
+use crate::discord::interactions::update_resp_to_plain_content;
 
 use super::CREATE_RACE_CMD;
 
@@ -249,6 +247,51 @@ async fn handle_create_race(
     )))
 }
 
+fn run_started_components() -> Vec<Component> {
+    vec![
+        Component::ActionRow(ActionRow {
+        components: vec![
+            interactions::button_component(
+                "Finish run",
+                CUSTOM_ID_FINISH_RUN,
+                ButtonStyle::Success,
+            ),
+            interactions::button_component(
+                "Forfeit",
+                CUSTOM_ID_FORFEIT_RUN,
+                ButtonStyle::Danger,
+            ),
+        ],
+    })]
+}
+
+fn run_started_interaction_response(race_run: &RaceRun, preamble: Option<&str>) -> Result<InteractionResponse, String> {
+    let filenames = race_run.filenames()?;
+    let content = if let Some(p) = preamble {
+        format!("{}
+
+Good luck! your filenames are: `{}`
+
+If anything goes wrong, tell an admin there was an issue with run `{}`
+", p, filenames, race_run.uuid)
+    } else {
+        format!("Good luck! your filenames are: `{}`
+
+If anything goes wrong, tell an admin there was an issue with run `{}`
+", filenames, race_run.uuid)
+    };
+    let buttons = run_started_components();
+    Ok(InteractionResponse {
+        kind: InteractionResponseType::UpdateMessage,
+        data: Some(
+            InteractionResponseDataBuilder::new()
+                .components(buttons)
+                .content(content)
+                .build(),
+        ),
+    })
+}
+
 async fn handle_run_start(
     interaction: Box<MessageComponentInteraction>,
     state: &Arc<DiscordState>,
@@ -257,39 +300,11 @@ async fn handle_run_start(
     let mut rr = RaceRun::get_by_message_id(interaction.message.id, &state.pool)
         .await
         .map_err(|e| ErrorResponse::new(USER_FACING_ERROR, e))?;
-    let filenames = rr
-        .filenames()
-        .map_err(|e| ErrorResponse::new(USER_FACING_ERROR, e))?;
     rr.start();
     match rr.save(&state.pool).await {
         Ok(_) => {
-            let buttons = vec![Component::ActionRow(ActionRow {
-                components: vec![
-                    interactions::button_component(
-                        "Finish run",
-                        CUSTOM_ID_FINISH_RUN,
-                        ButtonStyle::Success,
-                    ),
-                    interactions::button_component(
-                        "Forfeit",
-                        CUSTOM_ID_FORFEIT_RUN,
-                        ButtonStyle::Danger,
-                    ),
-                ],
-            })];
-            let content = format!("Good luck! your filenames are: `{}`
-
-If anything goes wrong, tell an admin there was an issue with race `254bb3a6-5d23-4198-80bb-40f9994298c9`
-", filenames);
-            let resp = InteractionResponse {
-                kind: InteractionResponseType::UpdateMessage,
-                data: Some(
-                    InteractionResponseDataBuilder::new()
-                        .components(buttons)
-                        .content(content)
-                        .build(),
-                ),
-            };
+            let resp = run_started_interaction_response(&rr, None)
+                .map_err(|e| ErrorResponse::new(USER_FACING_ERROR, e))?;
             state
                 .interaction_client()
                 .create_response(interaction.id, &interaction.token, &resp)
@@ -299,18 +314,7 @@ If anything goes wrong, tell an admin there was an issue with race `254bb3a6-5d2
                 .map(|_| ())
         }
         Err(e) => {
-            println!("Error updating race run: {}", e);
-            let ir = interactions::update_resp_to_plain_content(
-                "There was an error starting your race. Please ping FoxLisk.",
-            );
-
-            state
-                .interaction_client()
-                .create_response(interaction.id, &interaction.token, &ir)
-                .exec()
-                .await
-                .map_err(|e| ErrorResponse::new(USER_FACING_ERROR, e))
-                .map(|_| ())
+            Err(ErrorResponse::new(USER_FACING_ERROR, format!("Error updating race run: {}", e)))
         }
     }
 }
@@ -353,35 +357,98 @@ where
     }
 }
 
-async fn handle_run_forfeit(
+async fn handle_run_forfeit_button(
     interaction: Box<MessageComponentInteraction>,
     state: &Arc<DiscordState>,
 ) -> Result<(), ErrorResponse> {
     const USER_FACING_ERROR: &str =
         "Something went wrong forfeiting this match. Please ping FoxLisk.";
-    if let Err(e) = update_race_run(
-        &interaction,
-        |rr| {
-            rr.forfeit();
-        },
-        state,
-    )
-    .await
-    {
-        Err(ErrorResponse::new(USER_FACING_ERROR, e))
-    } else {
-        let ir = interactions::update_resp_to_plain_content(
-            "You have forfeited this match. Please let the admins know if there are any issues.",
-        );
-        state
-            .interaction_client()
-            .create_response(interaction.id, &interaction.token, &ir)
-            .exec()
-            .await
-            .map_err(|e| ErrorResponse::new(USER_FACING_ERROR, e))
-            .map(|_| ())
-    }
+
+    let ir = create_modal(
+        CUSTOM_ID_FORFEIT_MODAL,
+        "Forfeit",
+        "Enter \"forfeit\" if you want to forfeit",
+        vec![Component::TextInput(TextInput {
+            custom_id: CUSTOM_ID_FORFEIT_MODAL_INPUT.to_string(),
+            label: "Type \"forfeit\" to forfeit.".to_string(),
+            max_length: Some(7),
+            min_length: Some(7),
+            placeholder: None,
+            required: Some(true),
+            style: TextInputStyle::Short,
+            value: None,
+        })],
+    );
+    state
+        .interaction_client()
+        .create_response(interaction.id, &interaction.token, &ir)
+        .exec()
+        .await
+        .map_err(|e| ErrorResponse::new(USER_FACING_ERROR, e))
+        .map(|_| ())
 }
+
+
+lazy_static! {
+    static ref FORFEIT_REGEX: regex::Regex =
+        regex::RegexBuilder::new(r"\s*forfeit\s*").case_insensitive(true).build().unwrap();
+}
+
+async fn handle_run_forfeit_modal(
+    mut interaction: Box<ModalSubmitInteraction>,
+    state: &Arc<DiscordState>,
+) -> Result<(), ErrorResponse> {
+    const USER_FACING_ERROR: &str = "Something went wrong forfeiting this race. Please ping FoxLisk.";
+    let ut = get_field_from_modal_components(
+        std::mem::take(&mut interaction.data.components),
+        CUSTOM_ID_FORFEIT_MODAL_INPUT,
+    )
+        .ok_or(ErrorResponse::new(
+            USER_FACING_ERROR,
+            "Error getting forfeit input from modal.",
+        ))?;
+    let ir = if FORFEIT_REGEX.is_match(&ut) {
+        update_race_run(&interaction, |rr| rr.forfeit(), state)
+            .await
+            .map_err(|e| {
+                ErrorResponse::new(
+                    USER_FACING_ERROR,
+                    e,
+                )
+            })?;
+
+        update_resp_to_plain_content(
+            "You have forfeited this match. Please let the admins know if there are any issues.")
+    } else {
+        let mid = match interaction.get_message_id() {
+            Some(id) => id,
+            None => {
+                return Err(
+                    ErrorResponse::new(USER_FACING_ERROR,
+                    format!("Interaction {:?} has no message id", interaction)
+                    )
+                );
+            }
+        };
+
+        RaceRun::get_by_message_id(mid, &state.pool).await
+            .and_then(|race_run| run_started_interaction_response(&race_run, Some("Forfeit canceled")))
+            .map_err(|e| ErrorResponse::new(USER_FACING_ERROR, e))?
+    };
+    state
+        .interaction_client()
+        .create_response(interaction.id, &interaction.token, &ir)
+        .exec()
+        .await
+        .map_err(|e| {
+            ErrorResponse::new(
+                USER_FACING_ERROR,
+                e.to_string(),
+            )
+        })
+        .map(|_| ())
+}
+
 
 fn create_modal(
     custom_id: &str,
@@ -517,7 +584,7 @@ async fn handle_vod_ready(
         "Please enter your VoD URL",
         "VoD URL",
         vec![Component::TextInput(TextInput {
-            custom_id: CUSTOM_ID_VOD.to_string(),
+            custom_id: CUSTOM_ID_VOD_MODAL_INPUT.to_string(),
             label: "Enter VoD here".to_string(),
             max_length: None,
             min_length: Some(5),
@@ -547,7 +614,7 @@ async fn handle_vod_modal(
 ) -> Result<(), ErrorResponse> {
     let user_input = get_field_from_modal_components(
         std::mem::take(&mut interaction.data.components),
-        CUSTOM_ID_VOD,
+        CUSTOM_ID_VOD_MODAL_INPUT,
     )
     .ok_or(ErrorResponse::new(
         "Something went wrong reporting your VoD. Please ping FoxLisk.",
@@ -585,7 +652,7 @@ async fn handle_button_interaction(
 ) -> Result<(), ErrorResponse> {
     match interaction.data.custom_id.as_str() {
         CUSTOM_ID_START_RUN => handle_run_start(interaction, state).await,
-        CUSTOM_ID_FORFEIT_RUN => handle_run_forfeit(interaction, state).await,
+        CUSTOM_ID_FORFEIT_RUN => handle_run_forfeit_button(interaction, state).await,
         CUSTOM_ID_FINISH_RUN => handle_run_finish(interaction, state).await,
         CUSTOM_ID_VOD_READY => handle_vod_ready(interaction, state).await,
         _ => {
@@ -602,6 +669,7 @@ async fn handle_modal_submission(
     match interaction.data.custom_id.as_str() {
         CUSTOM_ID_USER_TIME_MODAL => handle_user_time_modal(interaction, state).await,
         CUSTOM_ID_VOD_MODAL => handle_vod_modal(interaction, state).await,
+        CUSTOM_ID_FORFEIT_MODAL => handle_run_forfeit_modal(interaction, state).await,
         _ => {
             println!("Unhandled modal: {:?}", interaction);
             Ok(())
