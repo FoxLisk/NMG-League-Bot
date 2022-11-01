276fe400b49c737a08229cb66b7a4a4b9ab580c9
Alexander <ancorwin@gmail.com>
2022-10-31 18:14:28 -0700

    fix commportunities posts, maybe

---
+++ src/bin/fix_commportunities_posts.rs
@@ -0,0 +1,55 @@
+use nmg_league_bot::db::raw_diesel_cxn_from_env;
+use nmg_league_bot::models::bracket_race_infos::BracketRaceInfo;
+use nmg_league_bot::models::season::Season;
+use nmg_league_bot::schema::bracket_race_infos;
+use diesel::prelude::*;
+use swiss_pairings::Player;
+use twilight_http::Client;
+use twilight_model::channel::embed::Embed;
+use twilight_model::id::Id;
+use twilight_model::id::marker::MessageMarker;
+use twilight_util::builder::embed::EmbedFooterBuilder;
+use nmg_league_bot::ChannelConfig;
+use nmg_league_bot::constants::TOKEN_VAR;
+use nmg_league_bot::utils::{env_var, race_to_nice_embeds};
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().unwrap();
+    let mut db = raw_diesel_cxn_from_env().unwrap();
+    let infos: Vec<BracketRaceInfo> = bracket_race_infos::table.load(&mut db).unwrap();
+
+    let client = Client::new(env_var(TOKEN_VAR));
+    let channel_config = ChannelConfig::new_from_env();
+
+    for info in infos {
+        println!("info: {:?}", info);
+        let msg_id = match info.get_commportunities_message_id() {
+            None => {continue;}
+            Some(m) => {m}
+        };
+        let fields = race_to_nice_embeds(&info, &mut db).map_err(|e| e.to_string()).unwrap();
+        let embeds = vec![Embed {
+            author: None,
+            color: Some(0x00b0f0),
+            description: None,
+            fields,
+            footer: Some(EmbedFooterBuilder::new("React to volunteer").build()),
+            image: None,
+            kind: "rich".to_string(),
+            provider: None,
+            thumbnail: None,
+            timestamp: None,
+            title: Some(format!("New match available for commentary")),
+            url: None,
+            video: None,
+        }];
+        let msg = client
+            .update_message(channel_config.commportunities, msg_id)
+            .embeds(Some(&embeds)).unwrap()
+            .exec()
+            .await
+            .unwrap();
+        println!("{msg:?}");
+    }
+}
