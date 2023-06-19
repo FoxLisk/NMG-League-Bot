5394699c9bbd3722d5a691ba10d09eaa7f10db95
RequiemOfSpirit <requiemofspirit2477@gmail.com>
2023-06-19 00:12:55 -0700

    feat: Update top navbar with custom styles

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ NGINX_TARGET_PATH="/etc/nginx/conf.d/$SERVICE_NAME.conf"
 TIMESTAMP=$(date +%s)
 cp -a db "db.$TIMESTAMP"
 cargo build
+npx sass http/stylesheets/:http/static/css/
 npx -y tailwindcss -i ./tailwind_input.css -o ./http/static/css/tailwind.css
 sudo cp "conf_files/lib/systemd/system/$SERVICE_NAME.service" $SERVICE_PATH
 if [ -f $NGINX_TARGET_PATH ]; then
