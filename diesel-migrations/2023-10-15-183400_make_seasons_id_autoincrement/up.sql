6337c41b50b038f5c61e74bea5db845ccf249d77
Alexander <ancorwin@gmail.com>
2023-10-15 11:44:48 -0700

    make season id autoincrement
    this allows me to burn a db id so people will shut the fuck up about my URLs

---
+++ diesel-migrations/2023-10-15-183400_make_seasons_id_autoincrement/up.sql
@@ -0,0 +1,25 @@
+-- Your SQL goes here
+ROLLBACK;
+PRAGMA foreign_keys=OFF;
+BEGIN;
+CREATE TABLE __new_seasons
+(
+   id                   INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+   started              BIGINT NOT NULL,
+   finished             BIGINT NULL,
+   format               TEXT NOT NULL,
+   state                TEXT NOT NULL DEFAULT '"Created"',
+   rtgg_category_name   TEXT NOT NULL DEFAULT "alttp",
+   rtgg_goal_name       TEXT NOT NULL DEFAULT "Any% NMG"
+);
+
+INSERT INTO __new_seasons(id, started, finished, format, state, rtgg_category_name, rtgg_goal_name)
+SELECT                    id, started, finished, format, state, rtgg_category_name, rtgg_goal_name
+FROM seasons;
+
+
+DROP TABLE seasons;
+ALTER TABLE __new_seasons RENAME TO seasons;
+
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
